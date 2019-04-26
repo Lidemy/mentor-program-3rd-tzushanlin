3f7c4e6e0a021c26cf5befd43b200e868a4f0765
Sandra Lin <tzushanlin@gmail.com>
2019-04-26 15:12:22 +0800

    hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,4 +1,9 @@
 function capitalize(str) {
+  let capitalFirst = ' ';
+  if (str[0] >= 'a' && str[0] <= 'z') {
+    capitalFirst = str[0].toUpperCase() + str.substring(1);
+    return capitalFirst;
+  }
   return str;
 }
 
